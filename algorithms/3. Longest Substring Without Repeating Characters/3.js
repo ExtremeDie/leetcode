3dc50570b5b630b778b8ecb2ccbebbd4a05a4da6
GitButler <gitbutler@gitbutler.com>
2024-07-15 22:22:56 +0800

    refactor: Update solution for finding longest substring without repeating characters.

--- algorithms/3. Longest Substring Without Repeating Characters/3.js
+++ algorithms/3. Longest Substring Without Repeating Characters/3.js
@@ -8,7 +8,7 @@ var lengthOfLongestSubstring = function (s) {
 	// keeps track of the starting index of the current substring.
 	let left = 0;
 
-	return s.split("").reduce((prev, value, index) => {
+	return s.split('').reduce((prev, value, index) => {
 		// starting index of substring is 1 + (the last index of this letter) to ensure this letter is not counted twice.
 		left = map[value] >= left ? map[value] + 1 : left;
 		// updates last recorded index of letter to the most recent index.
@@ -18,4 +18,24 @@ var lengthOfLongestSubstring = function (s) {
 		// +1 because if your substring starts and ends at index 0, it still has a length of 1.
 		return Math.max(prev, index - left + 1);
 	}, 0);
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+	let set = new Set();
+	let l = 0;
+	let maxLength = 0;
+
+	for (let r = 0; r < s.length; r++) {
+		while (set.has(s[r])) {
+			set.delete(s[l]);
+			l++;
+		}
+		set.add(s[r]);
+		maxLength = Math.max(maxLength, r - l + 1);
+	}
+	return maxLength;
 };
