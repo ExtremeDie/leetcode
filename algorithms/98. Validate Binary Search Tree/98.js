fe1f481430ff29901f36d5d62b0068761d1dcfd4
ExtremeDie <poringkyz05059@gmail.com>
2024-08-24 19:15:07 +0800

    feat: Add solution for validating Binary Search Tree with BST definition in README and JavaScript implementation.

---
+++ algorithms/98. Validate Binary Search Tree/98.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+	return isValid(root, -Infinity, Infinity);
+};
+
+var isValid = function (node, left, right) {
+	if (!node) return true;
+
+	if (!(node.val > left && node.val < right)) return false;
+	return isValid(node.left, left, node.val) && isValid(node.right, node.val, right);
+};
