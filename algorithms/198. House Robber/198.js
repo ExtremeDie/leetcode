103c9c2228f021cc7812b63ee276dd58559f94f4
ExtremeDie <poringkyz05059@gmail.com>
2024-08-26 21:46:03 +0800

    refactor: Remove unnecessary files, add new file, and update House Robber algorithm

--- algorithms/198. House Robber/198.js
+++ algorithms/198. House Robber/198.js
@@ -13,3 +13,18 @@ var rob = function (nums) {
 	}
 	return Math.max(rob, noRob);
 };
+var rob = function (nums) {
+	const n = nums.length;
+	if (n === 1) {
+		return nums[0];
+	}
+	const dp = new Array(n).fill(0);
+	dp[0] = nums[0];
+	dp[1] = Math.max(nums[0], nums[1]);
+
+	for (let i = 2; i < n; i++) {
+		dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+	}
+
+	return dp[n - 1];
+};
