a5e509124990f08f7d735154aa60142ce5e54117
ExtremeDie <poringkyz05059@gmail.com>
2024-10-21 15:10:08 +0800

    feat: Add solution for trapping rain water problem

--- algorithms/167. Two Sum II - Input array is sorted/167.js
+++ algorithms/167. Two Sum II - Input array is sorted/167.js
@@ -14,3 +14,25 @@ var twoSum = function (numbers, target) {
 
 	return [start + 1, end + 1];
 };
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+	let start = 0;
+	let end = numbers.length - 1;
+
+	while (start < end) {
+		const sum = numbers[start] + numbers[end];
+		if (sum > target) {
+			end--;
+		} else if (sum < target) {
+			start++;
+		} else {
+			return [start + 1, end + 1];
+		}
+	}
+	return [];
+};
