a5fd6d01e3c7f995c479556d6bbcce07c3ecd192
ExtremeDie <poringkyz05059@gmail.com>
2024-11-01 13:40:11 +0800

    refactor: Improve time and space complexity of climbStairs algorithm.

--- algorithms/70. Climbing Stairs/70.js
+++ algorithms/70. Climbing Stairs/70.js
@@ -13,3 +13,20 @@ var climbStairs = function (n) {
 	}
 	return one;
 };
+
+// Time complexity: O(n)
+// n is number of steps to reach the top.
+
+// Space complexity: O(n)
+// For dp list
+var climbStairs = function (n) {
+	const dp = new Array(n + 1);
+	dp[0] = 1;
+	dp[1] = 1;
+
+	for (let i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	return dp[n];
+};
