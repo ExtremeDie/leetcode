d9aa0541ec39e3110407250da3df9bbf5d089f2d
ExtremeDie <poringkyz05059@gmail.com>
2024-11-06 17:29:43 +0800

    feat: add solution for Subsets II problem
    Implement the solution for the Subsets II problem, which generates all 
    possible subsets of a given integer array that may contain duplicates. 
    Ensure that the solution set does not include duplicate subsets. Add 
    the problem description, examples, and constraints in the markdown 
    file for clarity.

---
+++ algorithms/90. Subsets II/90.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// Time: O(n * 2^n)
+// Space: O(n * 2^n)
+var subsetsWithDup = function (nums) {
+	const res = [];
+	nums.sort((a, b) => a - b);
+
+	function dfs(i, subset) {
+		if (i >= nums.length) {
+			res.push([...subset]);
+			return;
+		}
+
+		// include i
+		subset.push(nums[i]);
+		dfs(i + 1, subset);
+
+		// dont include i
+		subset.pop();
+
+		// skip all duplicates
+		while (i + 1 < nums.length && nums[i] === nums[i + 1]) {
+			i++;
+		}
+		dfs(i + 1, subset);
+	}
+	dfs(0, []);
+	return res;
+};
