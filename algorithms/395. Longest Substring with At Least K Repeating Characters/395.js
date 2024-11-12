24eb99f21e2b08b590ad51fd4e2b07f462104335
ExtremeDie <poringkyz05059@gmail.com>
2024-11-12 20:00:17 +0800

    feat: implement longest substring with at least k repeating characters
    Add a function to calculate the length of the longest substring in a 
    given string where each character appears at least k times. Include 
    etailed documentation and examples clarify usage and constraints

---
+++ algorithms/395. Longest Substring with At Least K Repeating Characters/395.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestSubstring = function (s, k) {
+	if (s.length < k) return 0;
+
+	let charCount = new Map();
+	for (let i = 0; i < s.length; i++) {
+		charCount.set(s[i], (charCount.get(s[i]) || 0) + 1);
+	}
+
+	// check if the strong already valid
+	let valid = true;
+	for (let count of charCount.values()) {
+		if (count < k) {
+			valid = false;
+			break;
+		}
+	}
+
+	if (valid) {
+		return s.length;
+	}
+
+	let start = 0;
+	let maxLength = 0;
+
+	for (let i = 0; i < s.length; i++) {
+		if (charCount.get(s[i]) < k) {
+			// search the start till before i
+			maxLength = Math.max(maxLength, longestSubstring(s.substring(start, i), k));
+			start = i + 1;
+		}
+	}
+
+	// check the remaining string
+	maxLength = Math.max(maxLength, longestSubstring(s.substring(start), k));
+	return maxLength;
+};
