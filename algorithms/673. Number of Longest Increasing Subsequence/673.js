210f19f39246d6c21f43d6ae69b2c4de29d26dd7
ExtremeDie <poringkyz05059@gmail.com>
2024-11-13 15:43:34 +0800

    feat: implement solution for longest increasing subsequence
    Add a function to calculate the number of longest increasing 
    subsequences in an array. The implementation uses dynamic 
    programming to efficiently determine the count of such 
    subsequences, addressing the problem requirements and 
    constraints outlined in the problem description.

---
+++ algorithms/673. Number of Longest Increasing Subsequence/673.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Time Complexity: O(n²)
+// Space Complexity: O(n)
+var findNumberOfLIS = function (nums) {
+	const dp = {}; // index -> [maxLIS, masCount]
+	let globalLIS = 0;
+	let globalCount = 0;
+
+	for (let i = nums.length - 1; i >= 0; i--) {
+		let maxLIS = 1;
+		let maxCount = 1;
+
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[j] > nums[i]) {
+				const [length, count] = dp[j];
+				if (length + 1 > maxLIS) {
+					maxLIS = length + 1;
+					maxCount = count;
+				} else if (length + 1 === maxLIS) {
+					maxCount += count;
+				}
+			}
+		}
+
+		if (maxLIS > globalLIS) {
+			globalLIS = maxLIS;
+			globalCount = maxCount;
+		} else if (maxLIS === globalLIS) {
+			globalCount += maxCount;
+		}
+
+		dp[i] = [maxLIS, maxCount];
+	}
+	return globalCount;
+};
