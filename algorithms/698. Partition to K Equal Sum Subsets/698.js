7399cc39a529e312f727025c101689a555e15c9d
ExtremeDie <poringkyz05059@gmail.com>
2024-11-14 22:24:17 +0800

    feat: add solution for partition to K equal sum subsets
    Implement the solution for the problem "Partition to K Equal Sum 
    Subsets" by adding the problem description and JavaScript function. The 
    function checks if an array can be divided into k subsets with equal 
    sums using a backtracking approach. This enhances the algorithm 
    collection and provides a complete solution this specific 
    problem.

---
+++ algorithms/698. Partition to K Equal Sum Subsets/698.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var canPartitionKSubsets = function (nums, k) {
+	const sum = nums.reduce((acc, curr) => acc + curr, 0);
+	if (sum % k) {
+		return false;
+	}
+
+	const target = sum / k;
+	nums.sort((a, b) => b - a); // descending order to speed up because of base case check
+	const visited = new Array(nums.length).fill(false);
+
+	function backtrack(i, k, subsetSum) {
+		if (k === 0) {
+			return true;
+		}
+		if (target === subsetSum) {
+			return backtrack(0, k - 1, 0);
+		}
+
+		for (let j = i; j < nums.length; j++) {
+			if (visited[j] || subsetSum + nums[j] > target) {
+				continue;
+			}
+
+			visited[j] = true;
+			if (backtrack(j + 1, k, subsetSum + nums[j])) {
+				return true;
+			}
+			visited[j] = false;
+		}
+		return false;
+	}
+
+	return backtrack(0, k, 0);
+};
