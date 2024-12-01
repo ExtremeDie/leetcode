571d67df221aa2db94070bcfe861b60dc2a7994d
ExtremeDie <poringkyz05059@gmail.com>
2024-12-01 14:20:23 +0800

    feat: add solution for Evaluate Reverse Polish Notation
    Implement the solution for the "Evaluate Reverse Polish Notation" problem. 
    Add a JavaScript function to evaluate expressions represented in 
    reverse Polish notation using a stack-based approach. Include 
    detailed problem description, constraints, and examples in the 
    Markdown file for clarity and reference.

---
+++ algorithms/150. Evaluate Reverse Polish Notation/150.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+	const stack = [];
+
+	for (const c of tokens) {
+		if (c === '+') {
+			stack.push(stack.pop() + stack.pop());
+		} else if (c === '-') {
+			stack.push(-stack.pop() + stack.pop());
+		} else if (c === '*') {
+			stack.push(stack.pop() * stack.pop());
+		} else if (c === '/') {
+			const a = stack.pop();
+			const b = stack.pop();
+			stack.push(Math.trunc(b / a));
+		} else {
+			stack.push(Number(c));
+		}
+	}
+	return stack[0];
+};
